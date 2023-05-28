c0c2bbc9970ac17e27e81995e7b34d00965769be
ChalidMeidy <chalidmeidy0702@gmail.comy>
2023-05-28 17:45:59 +0800

    Buat Method cetakInfo()

--- src/praktek03/Sepedaa.java
+++ src/praktek03/Sepedaa.java
@@ -4,4 +4,10 @@ public class Sepedaa {
     String merk;
     String warna;
     double harga;
+    
+    void cetakInfo () {
+        System.out.println("Merk \t: "+merk+"\n"+
+                "warna \t: "+warna+"\n"+
+                "Harga \t: "+harga);
+    }
 }
