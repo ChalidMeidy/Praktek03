f285b8b5264fbc29d95bafa793d6b13a3c25c1f8
ChalidMeidy <chalidmeidy0702@gmail.comy>
2023-05-28 17:53:51 +0800

    Instance Buat Attribute dan Panggil Method

--- src/praktek03/SepedaaAksi.java
+++ src/praktek03/SepedaaAksi.java
@@ -2,6 +2,11 @@ package praktek03;
 
 public class SepedaaAksi {
     public static void main(String[] args) {
+        Sepedaa s = new Sepedaa();
+        s.merk="Kupu-kupu";
+        s.warna="Merah";
+        s.harga=1000;
         
+        s.cetakInfo();
     }
 }
