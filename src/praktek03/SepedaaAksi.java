009c5873db8f44ef6f486a342dea450477a8154f
ChalidMeidy <chalidmeidy0702@gmail.comy>
2023-05-28 18:01:35 +0800

    Menggunakan Method Set

--- src/praktek03/SepedaaAksi.java
+++ src/praktek03/SepedaaAksi.java
@@ -3,9 +3,10 @@ package praktek03;
 public class SepedaaAksi {
     public static void main(String[] args) {
         Sepedaa s = new Sepedaa();
-        s.merk="Kupu-kupu";
-        s.warna="Merah";
-        s.harga=1000;
+
+        s.setMerk("Butterfly");
+        s.setWarna("Merah");
+        s.setHarga(1000);
         
         s.cetakInfo();
     }
